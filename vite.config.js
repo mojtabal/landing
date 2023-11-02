af28bdf38fbb334edc54268e38d2568e44c9811f
mojtaba tahmasb <hggvff222@gmail.com>
2023-11-02 10:33:01 +0330

    remove pwa

--- vite.config.js
+++ vite.config.js
@@ -1,61 +1,12 @@
-import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'prompt',
-      devOptions: { enabled: true },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'landingpage',
-        short_name: 'landingpage',
-        description: 'my landing project mojtaba)',
-        theme_color: '#ffffff',
-        start_url: '/',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
-      workbox: {
-        runtimeCaching: [{
-          urlPattern: ({ url }) => {
-            return url.pathname.startsWith('/api')
-          },
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'api-cache',
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }]
-      }
-    }),
-  ],
+  plugins: [react()],
   css: {
     modules: {
       localsConvention: 'camelCase'
     }
   }
-});
-
-
+})
